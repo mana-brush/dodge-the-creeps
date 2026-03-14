b25dc506b7b1c1d788a4fed48f61c8658f636246
David Cooper <dmcooper2412@gmail.com>
2026-03-14 09:38:38 -0700

    simple hud and resource image

--- scenes/main.gd
+++ scenes/main.gd
@@ -3,27 +3,26 @@ extends Node
 @export var mob_scene: PackedScene
 var score
 
-func _ready() -> void:
-	new_game()
-
 func game_over() -> void:
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 	
 func new_game():
 	score = 0
+	get_tree().call_group("mobs", "clear_queue")
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
-
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
 
 func _on_start_timer_timeout() -> void:
 	$MobTimer.start()
 	$ScoreTimer.start()
 
-
 func _on_score_timer_timeout() -> void:
 	score += 1
-
+	$HUD.update_score(score)
 
 func _on_mob_timer_timeout() -> void:
 	var mob = mob_scene.instantiate()
